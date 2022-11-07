1a266e9d0eb545b7420a59f3f1eb3d34f4fb2e30
Jaga <jagadheesh6@gmail.com>
2022-11-07 17:57:39 +0530

    test - prod docker 1

--- src/app/content/rank.content.ts
+++ src/app/content/rank.content.ts
@@ -2,7 +2,7 @@ import { VALORANT_ROLE_ID } from '../data/id';
 import { IGuild } from '../interface/shared.interface';
 
 export const RANK_MESSAGES = {
-	invalid_rank: ` PleasE enter a valid VALORANT rank!\nYou can use one of these: Unranked, Iron, Bronze, Silver, Gold, Platinum, Diamond, Ascendant, Immortal, Radiant\n
+	invalid_rank: ` Please enter a valid VALORANT rank!\nYou can use one of these: Unranked, Iron, Bronze, Silver, Gold, Platinum, Diamond, Ascendant, Immortal, Radiant\n
     Here's a valid Rank Role command ----- @kitty chan rank platinum`,
 
 	after_setRank: 'I have updated your VALORANT Rank. Check your new Role ;)'
