d1dac6db9cd3f6dc030c1650bca620004b6c32dd
Jaga <jagadheesh6@gmail.com>
2023-01-01 00:09:24 +0530

    feat - new year 2023

--- src/app/app.ts
+++ src/app/app.ts
@@ -38,7 +38,7 @@ export class App{
 	async start() {
 		///Connect to Discord Server
 		client.on('ready', () => {
-			client.user.setActivity('rock paper scissors', { type: ActivityType.Playing});
+			client.user.setActivity('to people\'s wishes!', { type: ActivityType.Listening});
 			console.log('kitty chan connected 😸');
 				
 		});
