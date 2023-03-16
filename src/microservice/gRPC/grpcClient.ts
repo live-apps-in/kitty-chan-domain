6b74b289febc4b966afcfcc475c4f194dbe5a579
Jagadheesh <jagadheeshm@wimoku.com>
2023-03-16 09:52:02 +0530

    fix - prod crash

--- src/microservice/gRPC/grpcClient.ts
+++ src/microservice/gRPC/grpcClient.ts
@@ -1,12 +1,12 @@
-import * as grpc from '@grpc/grpc-js';
-import { loadSync } from '@grpc/proto-loader';
-import { ProtoGrpcType } from '../../proto/live_cord';
-import 'dotenv/config';
-import { join } from 'path';
+// import * as grpc from '@grpc/grpc-js';
+// import { loadSync } from '@grpc/proto-loader';
+// import { ProtoGrpcType } from '../../proto/live_cord';
+// import 'dotenv/config';
+// import { join } from 'path';
 
-const PROTO_PATH = join(__dirname, '../../proto/live_cord.proto') ; // replace with your own proto file path
-const packageDefinition = loadSync(PROTO_PATH);
-const proto = (grpc.loadPackageDefinition(packageDefinition) as unknown) as ProtoGrpcType;
+// const PROTO_PATH = join(__dirname, '../../proto/live_cord.proto') ; // replace with your own proto file path
+// const packageDefinition = loadSync(PROTO_PATH);
+// const proto = (grpc.loadPackageDefinition(packageDefinition) as unknown) as ProtoGrpcType;
 
 
-export const liveCordgRPC = new proto.live_cord.GuildService(process.env.LIVE_CORD_GRPC_URL, grpc.credentials.createInsecure());
+// export const liveCordgRPC = new proto.live_cord.GuildService(process.env.LIVE_CORD_GRPC_URL, grpc.credentials.createInsecure());
