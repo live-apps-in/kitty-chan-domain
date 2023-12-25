2160285a6c208bbb5ddfbaafb82430a7c0ec2c61
Jaga <jagadheesh6@gmail.com>
2023-12-25 21:41:43 +0530

    fix - type issues in dto

--- src/modules/language/dto/language-filter.dto.ts
+++ src/modules/language/dto/language-filter.dto.ts
@@ -1,18 +1,18 @@
 import { FeatureDefault } from '../../../common/dto/features-default.dto';
 import { LanguageAction } from '../enum/language-filter.enum';
 
-export class LanguageFilterDto extends FeatureDefault {
-  public languageFilterConfig: LanguageFilterConfigDto[];
+export interface LanguageFilterDto extends FeatureDefault {
+  languageFilterConfig: LanguageFilterConfigDto[];
 }
 
-export class LanguageFilterConfigDto {
-  public isActive: boolean;
-  public languageLibId: string;
-  public actionConfig: LanguageFilterTriggerAction;
+export interface LanguageFilterConfigDto {
+  isActive: boolean;
+  languageLibId: string;
+  actionConfig: LanguageFilterTriggerAction;
 }
 
-class LanguageFilterTriggerAction {
-  public action: LanguageAction;
-  public emoji: string;
-  public plainMessage: string;
+interface LanguageFilterTriggerAction {
+  action: LanguageAction;
+  emoji: string;
+  plainMessage: string;
 }
